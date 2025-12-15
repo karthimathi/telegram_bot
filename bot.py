d635a324ccd934819cd56f2ab6b51651a932b53e
Karthick Kumar Mathivanan <76002821+karthimathi@users.noreply.github.com>
2025-12-15 21:51:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is missing!")
 
-async def start(update, context):
-    await update.message.reply_text("Bot is running ✅")
+# 👉 ADD YOUR DETAILS HERE
+CHANNEL_USERNAME = "https://t.me/techdealshuby"     # without @
+SUPPORT_USERNAME = "@mkarthick1997"        # without @
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("🔥 Join Channel", url=f"https://t.me/{CHANNEL_USERNAME}")],
+        [InlineKeyboardButton("💬 Support", url=f"https://t.me/{SUPPORT_USERNAME}")]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Welcome! Choose an option below 👇",
+        reply_markup=reply_markup
+    )
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
