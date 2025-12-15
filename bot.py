d17a9eaa8d90e88db12224d8a35c4db1b8ac3f37
Karthick Kumar Mathivanan <76002821+karthimathi@users.noreply.github.com>
2025-12-15 21:54:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("8438442286:AAFDLLAqI1zYgNaCQXZUVlXFNCpOEmBP6F0")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is missing!")
